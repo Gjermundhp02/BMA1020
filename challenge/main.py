a463be6cf5b3c9d91816df01ce795e96989268b3
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-03-01 19:25:15 +0100

    started with Tunnelcolor

--- challenge/main.py
+++ challenge/main.py
@@ -7,11 +7,16 @@ window = pyglet.window.Window(960, 540)
 batch = pyglet.graphics.Batch()
 
 # --- Globals ---
-backgroundColor = (0, 0, 0, 255)
+backgroundColor = (0.7, 0.7, 0.7, 1)
+colorInTunnel = (0, 0, 0, 1)
+colorOutTunnel = backgroundColor
 createTrack = False
-points = [(914, 495), (840, 391), (622, 460), (519, 425), (400, 450), (289, 466), (95, 245), (214, 71), (382, 213), (476, 327), (710, 336), (883, 321), (876, 167), (828, 106), (646, 143), (379, 94), (175, 111)]
+trackPoints = [(914, 495), (840, 391), (622, 460), (519, 425), (400, 450), (289, 466), (95, 245), (214, 71), (382, 213), (476, 327), (710, 336), (883, 321), (876, 167), (828, 106), (646, 143), (379, 94), (175, 111)]
+tunnelPoints = [(0.01, 0.1), (0.3, 0.4), (0.6, 0.7), (0.9, 0.99)]
 t=0
 # ----------------
+
+pyglet.gl.glClearColor(*backgroundColor)
 
 class Cart:
     def __init__(self, pos, size, origin=(), r=0, batch=None):
@@ -50,13 +55,14 @@ class FerrisWheel:
     def __init__(self, pos, r, thikness=5, batch=None):
         self.pos = np.array([*pos, 1])
         self.thikness = thikness
+        self.color = (0, 0, 0)
         # Two circles are used because Arc does not support thickness
-        self.shape = shapes.Circle(*pos, r, color=(255, 255, 255), batch=batch)
-        self.shape2 = shapes.Circle(*pos, r-self.thikness, color=backgroundColor, batch=batch)
-        self.legs = [shapes.Line(*pos, *(self.pos[:2]+[r/2, -r-10]), 5, color=(255, 255, 255), batch=batch),
-                     shapes.Line(*pos, *(self.pos[:2]+[-r/2, -r-10]), 5, color=(255, 255, 255), batch=batch)]
+        self.shape = shapes.Circle(*pos, r, color=self.color, batch=batch)
+        self.shape2 = shapes.Circle(*pos, r-self.thikness, color=[int(i*255) for i in backgroundColor], batch=batch)
+        self.legs = [shapes.Line(*pos, *(self.pos[:2]+[r/2, -r-10]), 5, color=self.color, batch=batch),
+                     shapes.Line(*pos, *(self.pos[:2]+[-r/2, -r-10]), 5, color=self.color, batch=batch)]
 
-        self.spokes = [shapes.Line(*pos, r*np.cos(2*np.pi/8*i)+pos[0], r*np.sin(2*np.pi/8*i)+pos[1], 2, color=(255, 255, 255), batch=batch) for i in range(8)]
+        self.spokes = [shapes.Line(*pos, r*np.cos(2*np.pi/8*i)+pos[0], r*np.sin(2*np.pi/8*i)+pos[1], 2, color=self.color, batch=batch) for i in range(8)]
         self.carts = [Cart(self.pos+[r-thikness/2, 0, 0], (20, 10), self.pos, 2*np.pi/8*i, batch) for i in range(8)]
 
     def update(self):
@@ -65,6 +71,29 @@ class FerrisWheel:
             cart.update()
         for spoke in self.spokes:
             spoke.x2, spoke.y2 = rotate((spoke.x2, spoke.y2, 1), self.pos, 0.01)[:2]
+        self.shape2.color = [int(i*255) for i in backgroundColor]
+
+class Tunnel:
+    def __init__(self, t, width=1, batch=None):
+        self.tpos = np.array(t)
+        self.dir = 1
+        self.tmid = (t[1]-t[0])/2+t[0]
+        self.pos1 = np.array(translateList(trackPoints, t[0])[0])
+        self.pos2 = np.array(translateList(trackPoints, t[1])[0])
+        self.width = width
+        self.shape = shapes.Line(*self.pos1, *self.pos2, width, color=(255, 0, 0), batch=batch)
+        self.t = 0
+
+    def update(self, dt):
+        global t, backgroundColor
+        # store previous t to check if t has crossed tmid
+        self.t += (self.tpos[1]-self.tpos[0])*(1/self.tpos[1])/2*dt
+        if self.tpos[0]<t<self.tmid:
+            backgroundColor = translate(colorOutTunnel, colorInTunnel, self.t)
+            print(backgroundColor)
+        elif self.tpos[1]>t>self.tmid:
+            backgroundColor = translate(colorInTunnel, colorOutTunnel, self.t)
+        
 
 def rotate(pos, origin, t):
     rotation = np.array([[np.cos(t), -np.sin(t), 0],
@@ -80,21 +109,28 @@ def rotate(pos, origin, t):
     return (translation @ rotation @ translationNeg @ pos)
 
 def translate(p1, p2, t):
-    return ((1-t)*p1[0]+t*p2[0], (1-t)*p1[1]+t*p2[1])
+    if len(p1)==4 and len(p2)==4:
+        return((1-t)*p1[0]+t*p2[0], (1-t)*p1[1]+t*p2[1], (1-t)*p1[2]+t*p2[2], (1-t)*p1[3]+t*p2[3])
+    else:
+        return ((1-t)*p1[0]+t*p2[0], (1-t)*p1[1]+t*p2[1])
 
 def translateList(points, t):
-        return [translate(points[i], points[i+1], t) for i in range(len(points)-1)]
+        if len(points)<2:
+            return points
+        else:
+            return translateList([translate(points[i], points[i+1], t) for i in range(len(points)-1)], t=t)
 
 # --- Globals instances ---
-track = shapes.BezierCurve(*points, color=(255, 255, 255), batch=batch)
-cart = Cart(points[0], (10, 5), batch=batch)
+track = shapes.BezierCurve(*trackPoints, color=(0, 0, 0), batch=batch)
+cart = Cart(trackPoints[0], (10, 5), batch=batch)
 wheel = FerrisWheel((100, 100), 50, batch=batch)
+tunnels = [Tunnel(tunnelPoints[i], 10, batch) for i in range(len(tunnelPoints))]
 # --------------------------
 
 # --- Curve creation ---
 if createTrack:
     track.delete()
-    points.clear()
+    trackPoints.clear()
 # ----------------------
 
 @window.event
@@ -102,10 +138,9 @@ def on_mouse_release(x, y, button, modifiers):
     # --- Curve creation ---
     if createTrack:
         global track
-        points.append((x, y))
-        print(points)
-        if len(points) > 2:
-            track = shapes.BezierCurve(*points, color=(255, 255, 255), batch=batch)
+        trackPoints.append((x, y))
+        if len(trackPoints) > 2:
+            track = shapes.BezierCurve(*trackPoints, color=(0, 0, 0), batch=batch)
     # ----------------------
 
 @window.event
@@ -113,7 +148,7 @@ def on_key_press(symbol, modifiers):
     # --- Curve creation ---
     if createTrack:
         if symbol == key.C:
-            points.clear()
+            trackPoints.clear()
             track.delete()
     # ----------------------
 
@@ -121,12 +156,14 @@ def update(dt):
     global t
     t += 0.05*dt
     t%=1
-    plist = points.copy()
-    while len(plist)>1:
-        plist = translateList(plist, t)
+    plist = trackPoints.copy()
+    plist = translateList(plist, t)
     cart.setPos(plist[0])
     cart.update()
     wheel.update()
+    for tunnel in tunnels:
+        tunnel.update(dt)
+    pyglet.gl.glClearColor(*backgroundColor)
 
 
 pyglet.clock.schedule_interval(update, 1/60)
