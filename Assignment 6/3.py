7472a1ab37ff2166fb4b473f5557a022eafd5c9c
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-03-07 23:51:10 +0100

    Finnished task 3

--- Assignment 6/3.py
+++ Assignment 6/3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import timeit
 
 np.random.seed(5)
 M = np.random.rand(20,20)
@@ -6,25 +7,19 @@ M[M <= 0.8] = 0.0
 M[M > 0.8] = 1.0
 
 # Compute the required matrix and store it in the variable answer
-lenM = [np.linalg.matrix_power(M,i) for i in range(20)]
-answer = np.zeros_like(M)
-#lenM[j]@(np.zeros(30)[:, :] := 1)
-for x in range(0, 2):
-    for y in range(0, 2):
-            av = np.zeros(20)
-            av[y] = 1
-            l = lenM[x]@av
-            print(l)
-            answer[y, l>0] =x
-#
-# for i in range(0, 2):
-#     sume = sum([lenM[j] for j in range(i+1)])
-#     print(sume)
-#     sume[sume>0] = i+1
-#     sume[sume==0] = 999
-#     answer = np.minimum(answer, sume)
+def my():
+    lenM = [np.linalg.matrix_power(M,i) for i in range(20)]
+    answer = np.zeros_like(M)
+    for i in range(0,19):
+        answer[answer == 0] = np.where(lenM[i][answer == 0] != 0, i, 0)
 
-# answer[answer==999] = 20
+def mark():
+    lenM = [np.linalg.matrix_power(M, i) for i in range(20)]
+    answer = np.zeros((20,20))
+    for i in range(0,19):
+        answer = np.array([[i if lenM[i][j][k] != 0 and answer[j][k] == 0 else answer[j][k] for k in range(20)] for j in range(20)])
+    answer[answer == 0] = 1
 
-print(M)
-print(answer)
+execs = 1000
+print(timeit.timeit(his, number=execs)/timeit.timeit(my, number=execs))
+
