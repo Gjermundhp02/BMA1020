7c87a3fd157557b0f425cf2af21ac56ed8f16444
gjermundhp02 <gjermund.pedersen@gamil.com>
2024-03-07 22:41:42 +0100

    task 3 test

--- Assignment 6/3.py
+++ Assignment 6/3.py
@@ -7,12 +7,24 @@ M[M > 0.8] = 1.0
 
 # Compute the required matrix and store it in the variable answer
 lenM = [np.linalg.matrix_power(M,i) for i in range(20)]
-answer = np.zeros_like(M)+999
-for i in range(0, 20):
-    sume = sum([lenM[j] for j in range(i+1)])
-    sume[sume>0] = i+1
-    sume[sume==0] = 999
-    answer = np.minimum(answer, sume)
+answer = np.zeros_like(M)
+#lenM[j]@(np.zeros(30)[:, :] := 1)
+for x in range(0, 2):
+    for y in range(0, 2):
+            av = np.zeros(20)
+            av[y] = 1
+            l = lenM[x]@av
+            print(l)
+            answer[y, l>0] =x
+#
+# for i in range(0, 2):
+#     sume = sum([lenM[j] for j in range(i+1)])
+#     print(sume)
+#     sume[sume>0] = i+1
+#     sume[sume==0] = 999
+#     answer = np.minimum(answer, sume)
 
-answer[answer==999] = 20
+# answer[answer==999] = 20
 
+print(M)
+print(answer)
