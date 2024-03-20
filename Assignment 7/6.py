e29540fb2c61649fb69339bbe56eb57bd222a956
gjermundhp02 <gjermund.pedersen@gamil.com>
2024-03-20 17:44:27 +0100

    Finnished task 6 and 7

--- Assignment 7/6.py
+++ Assignment 7/6.py
@@ -5,13 +5,16 @@ import math as m
 
 def integrate_ex1(f, start, stop, dt):
     return np.sum(np.vectorize(f)(np.arange(start, stop, dt))*dt)
-
+print(integrate_ex1(m.sin, 0, 5, 0.01))
 # b) 
 
 def integrate_ex2(f, start, stop, dt):
-    np.sum(np.vectorize(lambda x1, x2: f(x1)+f(x2)/2)(np.arange(start, stop, dt), np.arange(start+1, stop+1, dt))*dt)
+    return np.sum(np.vectorize(lambda x1, x2: (f(x1)+f(x2))/2)(np.arange(start, stop-dt, dt), np.arange(start+dt, stop, dt))*dt)
 print(int(100*integrate_ex2(m.sin, 1, 6, 0.02))/100)
 # c)
-
+# Simpson's 1/3 rule
 def integrate_ex3(f, start, stop, dt):
-    pass # delete this line before starting
+    n = 2
+    x = np.arange(start, stop, dt)
+    return (1/3)*((stop-start)/n)*(f(start)+4*np.sum(x[]))+2*np.sum(np.vectorize(f)(np.arange(start, n/2, 1)))+f(stop))
+print(integrate_ex3(m.sin, 2, 7, 0.03))
