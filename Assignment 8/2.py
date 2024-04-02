7cf33c87fc6e3e666334921902e9841e893e7a0d
gjermundhp02 <gjermund.pedersen@gmail.com>
2024-04-02 10:58:57 +0200

    Finished task 2

--- Assignment 8/2.py
+++ Assignment 8/2.py
@@ -49,11 +49,10 @@ def update(dt):
        t = np.clip(t, 0, 1)
        dir = -dir
     t += 0.2*dir*dt
-    print((1-t)*-WINDVEL+t*WINDVEL)
     new = np.array([[np.random.randint(200, 300), np.random.uniform(np.pi/4, 3*np.pi/4), np.random.randint(10, 15)] for i in range(5)])
     particles = np.vstack((particles, new))
     shapes = np.hstack((shapes, [pyglet.shapes.Circle(WWIDTH/2, WHEIGHT/2, i[2], color=(255, 255, 255), batch=batch) for i in new]))
-    xVel = np.cos(particles[:, 1])*particles[:, 0]-(((1-t)*-WINDVEL+t*WINDVEL)*2*particles[:, 2]-particles[:, 0])*dt
+    xVel = np.cos(particles[:, 1])*particles[:, 0]-(((1-t)*-WINDVEL+t*WINDVEL)*2*particles[:, 2]-np.cos(particles[:, 1])*particles[:, 0])*dt
     yVel = np.sin(particles[:, 1])*particles[:, 0]-200*dt
     particles[:, 0] = np.linalg.norm(np.vstack((xVel, yVel)), axis=0)
     particles[:, 1] = np.arctan2(yVel, xVel)
