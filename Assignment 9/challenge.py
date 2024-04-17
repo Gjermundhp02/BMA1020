023f229ceaa8c2b8531be03ff5a3b66f74ee5a11
gjermundhp02 <gjermund.pedersen@gamil.com>
2024-04-17 19:11:12 +0200

    Challenge fixes

--- Assignment 9/challenge.py
+++ Assignment 9/challenge.py
@@ -9,7 +9,7 @@ WHEIGHT = 540
 window = pyglet.window.Window(WWIDTH, WHEIGHT)
 batch = pyglet.graphics.Batch()
 
-K = 2
+K = 9
 g = 10
 
 def genVec(n):
@@ -21,38 +21,41 @@ def genVec(n):
 
 class SoftBody:
     def __init__(self, pos, vel, size):
-        self.pos = pos
-        self.vel = vel
         self.size = size
         self.points = {
-            "points": (p:=np.array([pos, pos+np.array([size, 0]), pos+np.array([size, size]), pos+np.array([0, size])])),
-            "shapes": np.array([shapes.Circle(i[0], i[1], 5, color=(255, 255, 255), batch=batch) for i in p])
+            "points": (p:=np.array([[pos, vel, [0, 0]], [pos+np.array([size, 0]), vel, [0, 0]], [pos+np.array([size, size]), vel, [0, 0]], [pos+np.array([0, size]), vel, [0, 0]]], dtype=np.float64)),
+            "shapes": np.array([shapes.Circle(i[0], i[1], 5, color=(255, 255, 255), batch=batch) for i in p[:, 0]])
         }
         self.lines = {
-            "lines": (l:=np.hstack((v:=genVec(4), np.linalg.norm(p[v[:, 0]]-p[v[:, 1]], axis=1)[:, np.newaxis]))),
-            "shapes": np.array([shapes.Line(*p[int(i)], *p[int(j)], color=(255, 255, 255), batch=batch) for i, j, _ in l])
+            "lines": (l:=np.hstack((v:=genVec(4), np.linalg.norm(p[v[:, 0], 0]-p[v[:, 1], 0], axis=1)[:, np.newaxis]))),
+            "shapes": np.array([shapes.Line(*p[int(i), 0], *p[int(j), 0], color=(255, 255, 255), batch=batch) for i, j, _ in l])
         }
 
     def update(self, dt):
-        self.points["points"][self.points["points"][:, 1] < 0, 1] = 0
-        vecs = self.points["points"][self.lines["lines"][:, 0].astype(np.int64)]-self.points["points"][self.lines["lines"][:, 1].astype(np.int64)]
-        dist = np.linalg.norm(vecs, axis=1)
-        norm = vecs/dist[:, np.newaxis]
-        distDiff = (self.lines["lines"][:, 2]-np.linalg.norm(vecs, axis=1))[:, np.newaxis]
+        self.points["points"][self.points["points"][:, 0, 1] < 0, 2, 1] += g*dt # Prevent buildup of speed while on the ground
+        self.points["points"][self.points["points"][:, 0, 1] < 0, 0, 1] = 0 # Keep in bounds y
+        vecs = self.points["points"][self.lines["lines"][:, 0].astype(np.int64), 0]-self.points["points"][self.lines["lines"][:, 1].astype(np.int64), 0] # Vectors between all the points
+        dist = np.linalg.norm(vecs, axis=1) # Distance between all the points
+        distDiff = (self.lines["lines"][:, 2]-dist)[:, np.newaxis] # Distance differance between what it should be and what it is
         if (distDiff>0.001).any() or (distDiff<-0.001).any(): # Needed because of floating point errors
-            acc = distDiff*K*dt*norm/2
-            print(acc, norm)
+            norm = vecs/dist[:, np.newaxis] # Normalized vectors between all the points
+            acc = distDiff*K*norm/2
+            print(norm, acc)
+            self.points["points"][self.lines["lines"][:, 0].astype(np.int64), 2] += acc # Add acceleration to the first point
+            self.points["points"][self.lines["lines"][:, 1].astype(np.int64), 2] -= acc # Subtract acceleration from the second point
 
-        self.vel = self.vel-[0, g*dt]
-        self.points["points"] = self.points["points"]+self.vel*dt
+        self.points["points"][:, 1] = self.points["points"][:, 1]+self.points["points"][:, 2]*dt+np.array([0, -g])*dt # Update velocity
+        self.points["points"][:, 0] = self.points["points"][:, 0]+self.points["points"][:, 1]*dt # Update position
+
+        # Update shapes
         for i in range(len(self.points["shapes"])):
-            self.points["shapes"][i].x, self.points["shapes"][i].y = self.points["points"][i]
+            self.points["shapes"][i].x, self.points["shapes"][i].y = self.points["points"][i, 0]
         for i in range(len(self.lines["shapes"])):
             i = int(i)
-            self.lines["shapes"][i].x, self.lines["shapes"][i].y = self.points["points"][int(self.lines["lines"][i, 0])]
-            self.lines["shapes"][i].x2, self.lines["shapes"][i].y2 = self.points["points"][int(self.lines["lines"][i, 1])]
+            self.lines["shapes"][i].x, self.lines["shapes"][i].y = self.points["points"][int(self.lines["lines"][i, 0]), 0]
+            self.lines["shapes"][i].x2, self.lines["shapes"][i].y2 = self.points["points"][int(self.lines["lines"][i, 1]), 0]
 
-s1 = SoftBody(np.array([100, 100]), np.array([10, 0]), 100)
+s1 = SoftBody(np.array([100, 100]), np.array([0, 0]), 100)
 pyglet.clock.schedule_interval(s1.update, 1/60)
 
 @window.event
